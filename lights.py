7cc608be16f9753373bada50a496dc6c7916c0ac
Hans Johnson <hejohnson@wpi.edu>
2015-10-26 00:01:59 -0400

    Controls servo properly

--- lights.py
+++ lights.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
-import pigpio
+#import pigpio
 import email, imaplib, os, subprocess, smtplib
+import RPi.GPIO as GPIO
+import time
+#pi = pigpio.pi()
+#pi.set_PWM_frequency(14, 50)
 
-pi = pigpio.pi()
-pi.set_PWM_frequency(14, 50)
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+pwm = GPIO.PWM(14, 50)
+#pwm.start(25)
 
 detach_dir = "/home/ubuntu/foundersPrinting/files"
 
-f = open('userConfig.txt', 'r')
+f = open('/home/ubuntu/Code/RasPi-Lights/userConfig.txt', 'r')
 
 user = f.readline().strip()
 pwd = f.readline().strip()
@@ -19,31 +25,42 @@ smtp.login(user, pwd)
 m = imaplib.IMAP4_SSL('imap.gmail.com')
 m.login(user,pwd)
 m.select('Inbox')
-resp, items = m.search(None, "(BODY 'confirm')", "(UNSEEN)")
+resp, items = m.search(None, "(BODY 'Turn')", "(UNSEEN)")
 items = items[0].split()
 
 for emailid in items: # Iterates through email messages
-	copies = 1
-	doubleOption = ""
+#	print "found an email"
 	foundOptions = False
 	resp, data = m.fetch(emailid, "(RFC822)")
 	email_body = data[0][1]
 	mail = email.message_from_string(email_body)
 	origSender = mail["From"]
-	if mail.get_content_maintype() != 'multipart': # If the message doesn't have multiple parts, it can't have an attachment
+	if mail.get_content_maintype() == 'multipart': # If the message doesn't have multiple parts, it can't have an attachment
 		continue
 	
 	for part in mail.walk(): # Step through each part of the message
 		
 		if part.get_content_maintype() == 'text' and not foundOptions: # If it's the plaintext part, the main body, check for any options
-			print "body"
+#			print "body"
 			foundOptions = True
 			body = part.get_payload(decode=True).lower()
+#xy			print body
 			if "off" in body:
-								pi.set_PWM_dutycycle(14, 0)
+				pwm.start(5)
+				time.sleep(1.5)
+				pwm.ChangeDutyCycle(7.5)
+				time.sleep(1.5)
+				#pi.set_PWM_dutycycle(14, 0)
+				print "off"
 			elif "on" in body:
-								pi.set_PWM_dutycycle(14, 255)
-				continue
+				pwm.start(10)
+				time.sleep(1.5)
+				pwm.ChangeDutyCycle(7.5)
+				time.sleep(1.5)
+				#pi.set_PWM_dutycycle(14, 255)
+				print "on"
+			pwm.stop()
+			continue
 		
 		if part.get_content_maintype() == 'multipart': # if it's still a multipart, ignore it
 			continue
@@ -54,4 +71,5 @@ for emailid in items: # Iterates through email messages
 
 	m.store(emailid, '+FLAGS', '\Seen')
 m.logout()
+GPIO.cleanup()
 
